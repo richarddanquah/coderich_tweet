dfbe2b775e8286ddf3bb7c8f416840bff39b8833
Richard  Danquah <richpee60@yahoo.com>
2022-10-23 17:46:37 +0000

    new

--- lib/coderich_tweet/tweet.ex
+++ lib/coderich_tweet/tweet.ex
@@ -1,11 +1,11 @@
 defmodule CoderichTweet.Tweet do
   def send(str) do
-    ExTwitter.configure(:process,
-      consumer_key: System.get_env("CODERICH_TWEET_TWITTER_CONSUMER_KEY"),
-      consumer_secret: System.get_env("CODERICH_TWEET_TWITTER_CONSUMER_SECRET"),
-      access_token: System.get_env("CODERICH_TWEET_TWITTER_ACCESS_TOKEN"),
-      access_token_secret: System.get_env("CODERICH_TWEET_TWITTER_ACCESS_SECRET")
-    )
+    ExTwitter.configure(:process,[
+      consumer_key:" ",
+      consumer_secret: "",
+      access_token: "",
+      access_token_secret: ""
+    ])
 
     # token = ExTwitter.request_token("http://myapp.com/twitter-callback")
 
